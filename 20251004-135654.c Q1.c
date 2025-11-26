e6d7fdbd6c6bfa711b2da80c57398a1f24c90645
kkarthik01256-a11y <k.karthik01256@gmail.com>
2025-11-26 11:48:26 +0530

    Add files via upload

---
+++ 20251004-135654.c Q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main () 
+{
+int num1;
+int num2;
+printf("Enter the num1 value: \n");
+scanf("%d,&num1");
+printf("Enter the num2 value: \n");
+scanf("%d, &num2");
+int pro=num1=num2;
+printf("The pro of %d and %d is %d",num1,num2,pro);
+}
