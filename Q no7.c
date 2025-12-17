6cfcc0b771b2152922f08d65e2fed86b101c0bdf
kkarthik01256-a11y <k.karthik01256@gmail.com>
2025-12-17 21:53:18 +0530

    Add files via upload

---
+++ Q no7.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+void main()
+{
+ int name = " karthik ";
+ int age = 26;
+ int salary = 40000;
+ int section= "Cl";
+printf("The name is %s\n",name );
+printf("The age is %d\n", age);
+printf("The salary is %d\n",salary);
+printf("The section is %s\n",section);
+}
